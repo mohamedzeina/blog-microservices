6cae51e389b75e603c03d3fbe50a51ad12c04979
Mohamed Zeina <toxiczeina@gmail.com>
2026-03-14 19:10:50 +0300

    Add PostCreate component and render it in App

---
+++ client/src/PostCreate.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const PostCreate = () => {
+	const [title, setTtitle] = useState('');
+
+	const onSubmit = async (event) => {
+		event.preventDefault();
+
+		await axios.post('http://localhost:4000/posts', {
+			title,
+		});
+
+		setTtitle('');
+	};
+
+	return (
+		<div>
+			<form onSubmit={onSubmit}>
+				<div className="form-group">
+					<label>Title</label>
+					<input
+						value={title}
+						onChange={(e) => setTtitle(e.target.value)}
+						className="form-control"
+					/>
+				</div>
+				<button className="btn btn-primary">Submit</button>
+			</form>
+		</div>
+	);
+};
+
+export default PostCreate;
