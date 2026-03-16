7170c49f08cbcba64862150fbb1209bb7f48831c
Mohamed Zeina <toxiczeina@gmail.com>
2026-03-16 17:15:01 +0300

    Add CommentCreate component and render it in PostList

---
+++ client/src/CommentCreate.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { use } from 'react';
+
+const CommentCreate = ({ postId }) => {
+	const [comment, setComment] = useState('');
+
+	const handleSubmit = async (event) => {
+		event.preventDefault();
+		const response = await axios.post(
+			`http://localhost:4001/posts/${postId}/comments`,
+			{
+				content: comment,
+			},
+		);
+		setComment('');
+	};
+
+	return (
+		<div>
+			<form onSubmit={handleSubmit}>
+				<div className="form-group">
+					<label>New Comment</label>
+					<input
+						className="form-control"
+						onChange={(e) => setComment(e.target.value)}
+					/>
+				</div>
+				<button className="btn btn-primary">Submit</button>
+			</form>
+		</div>
+	);
+};
+
+export default CommentCreate;
