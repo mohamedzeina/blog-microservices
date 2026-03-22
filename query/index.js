efee988c553a368ae2ce088690dd5474a0d5c75c
Mohamed Zeina <toxiczeina@gmail.com>
2026-03-22 16:24:22 +0300

    Handle PostCreated and CommentCreated events in query service

--- query/index.js
+++ query/index.js
@@ -6,12 +6,31 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+const posts = {};
+
 app.get('/posts', (req, res) => {
-	res.send({});
+	res.send(posts);
 });
 
 app.post('/events', (req, res) => {
-	console.log('Received event:', req.body.type);
+	const { type, data } = req.body;
+
+	if (type === 'PostCreated') {
+		const { id, title } = data;
+		posts[id] = {
+			id,
+			title,
+			comments: [],
+		};
+	}
+	if (type === 'CommentCreated') {
+		const { id, content, postId } = data;
+		const post = posts[postId];
+		if (post) {
+			post.comments.push({ id, content });
+		}
+	}
+
 	res.send({});
 });
 
