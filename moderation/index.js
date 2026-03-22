1de6fbece098726485809931ff3739bc5109747e
Mohamed Zeina <toxiczeina@gmail.com>
2026-03-22 17:21:21 +0300

    Add comment moderation flow with pending and approval statuses

--- moderation/index.js
+++ moderation/index.js
@@ -6,8 +6,27 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
 	const { type, data } = req.body;
+	if (type === 'CommentCreated') {
+		const status = data.content.includes('orange') ? 'rejected' : 'approved';
+		await axios.post(
+			'http://localhost:4005/events',
+			{
+				type: 'CommentModerated',
+				data: {
+					id: data.id,
+					content: data.content,
+					postId: data.postId,
+					status,
+				},
+			}.catch((err) => {
+				console.log(err.message);
+			}),
+		);
+
+		res.send({});
+	}
 });
 
 app.listen(4003, () => {
