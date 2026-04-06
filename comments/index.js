eace96a225c6b8ad804b96e70b0485a6f3b02735
Mohamed Zeina <toxiczeina@gmail.com>
2026-04-06 15:04:06 +0300

    Update remaining services to use Kubernetes cluster DNS hostnames

--- comments/index.js
+++ comments/index.js
@@ -25,7 +25,7 @@ app.post('/posts/:id/comments', async (req, res) => {
 	commentByPostId[req.params.id] = comments;
 
 	await axios
-		.post('http://localhost:4005/events', {
+		.post('http://event-bus-clusterip-srv:4005/events', {
 			type: 'CommentCreated',
 			data: {
 				id: commentId,
@@ -54,7 +54,7 @@ app.post('/events', async (req, res) => {
 		comment.status = status;
 
 		await axios
-			.post('http://localhost:4005/events', {
+			.post('http://event-bus-clusterip-srv:4005/events', {
 				type: 'CommentUpdated',
 				data: {
 					id,
